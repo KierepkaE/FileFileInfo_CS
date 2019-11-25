18ccc70276345808e9cc82b7b79b186e9e59059f
kierepkae <kierepkaedyta@gmail.com>
2019-11-25 10:59:48 +0100

    Initial check-in of module FileFileInfo

---
+++ FileFileInfo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FileFileInfo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello File System!");
+        }
+    }
+}
